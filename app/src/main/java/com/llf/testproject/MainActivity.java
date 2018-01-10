ef5731ae65e60fb628f8f0f4c0e38f786ad132ec
yh_gao <stayli117@163.com>
2018-01-10 10:26:38 +0800

    使用JDK 实现观察者模式

--- app/src/main/java/com/llf/testproject/MainActivity.java
+++ app/src/main/java/com/llf/testproject/MainActivity.java
@@ -1,85 +1,69 @@
 package com.llf.testproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.webkit.JavascriptInterface;
-import android.webkit.JsResult;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.zryf.sotp.KeyBoardDialog;
-import com.zryf.sotp.global.KeyBoardInputCallback;
+import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity implements KeyBoardInputCallback {
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    private KeyBoardDialog keyboard;
 
+    private static final String TAG = "MainActivity";
+    private TextView mTvContent;
+    private Button mBtnNet;
+    private Handler mHandler;
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-    }
-
-    private void initView() {
-        webView = (WebView) findViewById(R.id.webview);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        mHandler = new Handler();
+        mTvContent = (TextView) findViewById(R.id.tv_content);
+        mBtnNet = (Button) findViewById(R.id.btn_net);
+        mBtnNet.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                getPwd("123456");
+            public void onClick(View v) {
+                mTvContent.setText("网络请求中");
+                OkHttpClient client = new OkHttpClient.Builder().build();
+                client.newCall(new Request.Builder().url("http://www.baidu.com").build())
+                        .enqueue(new Callback() {
+                            @Override
+                            public void onFailure(Call call, IOException e) {
+                                mHandler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mTvContent.setText("fail");
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onResponse(Call call, Response response) throws IOException {
+                                mHandler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mTvContent.setText("ok");
+                                    }
+                                });
+                            }
+                        });
             }
         });
-
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);// 设置支持javascript脚本
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-                Log.e("People", "onJsAlert:" + message);
-                return super.onJsAlert(view, url, message, result);
-            }
-
-            @Override
-            public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
-                Log.e("People", "onJsConfirm:" + message);
-                return super.onJsConfirm(view, url, message, result);
-            }
-        });
-        webView.setWebViewClient(new WebViewClient(){
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-            }
-        });
-        webView.addJavascriptInterface(this, "my");
-        webView.loadUrl("file:///android_asset/index.html");
     }
 
-    @JavascriptInterface
-    public void showKeyboard() {
-//        keyboard = new KeyBoardDialog(this, this, "", 0);
-//        keyboard.show();
-        Toast.makeText(this, "show", Toast.LENGTH_SHORT).show();
-
-
-    }
-
-    @Override
-    public void getPwd(String pwd) {
-        if (!TextUtils.isEmpty(pwd)) {
-            webView.loadUrl("javascript:native2JsData" + "('" + pwd + "')");
-        }
-    }
-
-    @Override
-    public void getCharNum(int num) {
-
-    }
 }
